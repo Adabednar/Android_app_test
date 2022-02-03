753b1608b4ad86f3d1fa112348c06b147380c78a
Adabednar <bed0070@slu.cz>
2022-02-03 12:47:45 +0100

    Additional array tests

--- Test_app/app/src/test/java/com/example/test_app/OddArrayTest.java
+++ Test_app/app/src/test/java/com/example/test_app/OddArrayTest.java
@@ -2,12 +2,14 @@ package com.example.test_app;
 
 import static org.junit.Assert.*;
 
+import android.os.Build;
+
 import org.junit.Test;
 
 public class OddArrayTest {
 
     @Test
-    public void IsResultCorrect() {
+    public void ArrayIsMixed() {
         int[] arr = {1,5,15,192,137,180};
         int expected = 158;
 
@@ -16,4 +18,28 @@ public class OddArrayTest {
 
         assertEquals(expected, output);
     }
+
+    @Test
+    public void ArrayIsEmpty() {
+        int[] arr = {};
+        int expected = 0;
+
+        OddArray oddArray = new OddArray();
+        int output = oddArray.ArrayOdds(arr);
+
+        assertEquals(expected, output);
+    }
+
+    @Test
+    public void ArrayIsEven() {
+        int[] arr = {2,6,18,192,138,180};
+        int expected = 0;
+
+        OddArray oddArray = new OddArray();
+        int output = oddArray.ArrayOdds(arr);
+
+        assertEquals(expected, output);
+    }
+
+
 }
