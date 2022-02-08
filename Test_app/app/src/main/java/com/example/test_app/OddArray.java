f63e57beb2f6fd634068504cc081277cc7cc19de
Marcel Klínek <marcel.klinek@ibillboard.com>
2022-02-08 09:33:47 +0100

    suggestions on code style and refactoring

--- Test_app/app/src/main/java/com/example/test_app/OddArray.java
+++ Test_app/app/src/main/java/com/example/test_app/OddArray.java
@@ -4,25 +4,31 @@ import java.util.Random;
 
 public class OddArray {
 
-    public int ArrayOddNumberSum(int[] arr){
+    public int getSumOfOddNumbers(int[] arr) {
         int result = 0;
         int arraySize = arr.length;
 
-        for(int i=0; i<arraySize; i++){
-            if(!(arr[i] % 2 == 0)){result = result + arr[i];}
+        for (int i=0; i<arraySize; i++) {
+            if (isNumberOdd(arr[i])) {
+                result = result + arr[i];
+            }
         }
         return result;
     }
 
-    public int[] RandomArray(int arraySize){
+    public int[] generateRandomNumbers(int arraySize) {
         int[] arr = new int[arraySize];
         Random r = new Random();
 
-        for(int i=0; i<arraySize; i++){
+        for (int i=0; i<arraySize; i++) {
             arr[i] = r.nextInt(10000);
         }
         return arr;
     }
 
+    private boolean isNumberOdd(int num) {
+        int remainderAfterDivision = num % 2
+        return remainderAfterDivision > 0
+    }
 
 }
